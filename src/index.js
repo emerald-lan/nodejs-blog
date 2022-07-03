d5812798eebef02c422c23682ffba32cef3809d8
Nttlan <emeraldlan1105@gmail.com>
2022-07-03 16:03:04 +0700

    add controllers and routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
-const {engine} = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 const path = require('path')
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }))
 app.use(express.json())
 
@@ -17,29 +19,14 @@ app.use(express.json())
 
 //Template engine
 app.engine('.hbs', engine({
-  extname: 'hbs'
+    extname: 'hbs'
 }))
-
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.get('/search', (req, res) => {
-  res.render('search')
-})
-
-app.post('/search', (req, res) => {
-  //console.log(req.body)
-  res.send('')
-})
+//Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
